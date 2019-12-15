b6a1f06df833f75c141ccaecda63dc78a925f33c
dongwook <dongwook224@naver.com>
2019-12-15 15:45:05 +0900

    구조 변경

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import React, { Component } from 'react'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import LeftBar from './components/leftbar/LeftBar'
-import MidBar from './components/midbar/MidBar'
-import RightBar from './components/rightbar/RightBar'
-import LeftLine from './components/leftline/LeftLine'
-
+import template from './components/template'
+import Dashboard from './components/Dashboard.jsx'
 
 class App extends Component {
+    state = template;
+
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col md={4}>
-                        <LeftBar />
-                    </Col>
-                    <Col md={4}>
-                        <MidBar />
-                    </Col>
-                    <Col md={4}>
-                        <RightBar />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={4}>
-                        <LeftLine />
-                    </Col>
-                    <Col md={4}>Line2</Col>
-                    <Col md={4}>Line3</Col>
-                </Row>
-                <Row>
-                    <Col md={8}>Graph1</Col>
-                    <Col md={4}>Table1</Col>
-                </Row>
-
-
-
-
-            </Container>
+            <Dashboard
+            userConfig={this.state.userConfig}
+            charts={this.state.charts}
+            />
         )
     }
 }
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Chart from './Chart';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div className="row">
+                {this.props.charts && 
+                   this.props.charts.map(chart => {
+                       return (
+                           <div className="col-xs-12 col-sm-6 mb-2">
+                               <Chart
+                               data={chart.series}
+                               userConfig={this.props.userConfig}
+                               titleName={chart.title}
+                    
+                               />
+                           </div>
+                       );
+                   })
+                   }
+
+            </div>
+        );
+    }
+}
+export default Dashboard;
