45c94bf59a39f72eee9425e1e43fa1f5c7fbe0c0
dongwook <dongwook224@naver.com>
2019-12-17 09:42:16 +0900

    dataProcessing, Selection 추가

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react'
-import template from './components/template'
-import Dashboard from './components/Dashboard.jsx'
+import React, { Component } from 'react';
+import template from './components/template';
+import Selection from './components/Selection';
+import Dashboard from './components/Dashboard.jsx';
+import dataProcessing, {
+    fossilFuelData,
+    hydroElectricData,
+    renewableEnergyData,
+    biomassData
+} from './components/dataProcessing'
 
 class App extends Component {
     state = template;
 
+    copyDataSeries = (obj = {}) => {
+
+    };
+
     render() {
         return (
-            <Dashboard
-            userConfig={this.state.userConfig}
-            charts={this.state.charts}
-            />
-        )
+            <>
+                <Selection
+                    yearFrom={this.state.yearFrom}
+                    yearTo={this.state.yearTo}
+                    onChangeYear={this.handleChangeYear}
+                    onSubmit={this.handleSubmit}
+                />
+                <Dashboard
+                    userConfig={this.state.userConfig}
+                    charts={this.state.charts}
+                />
+            </>
+        );
     }
 }
 
-export default App
+export default App;
