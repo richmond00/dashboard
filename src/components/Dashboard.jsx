9973050e143abfa0d15f05490ff1a65056abdac1
dongwook <dongwook224@gmail.com>
2019-12-24 23:23:43 +0900

    일반 부트스트랩 코드를 부트스크랩 리액트 코드로 변경

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
 import Chart from './Chart';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Dashboard extends Component {
     render() {
         return (
-            <div className="row">
+            <Row>
                 {this.props.charts && 
                    this.props.charts.map((chart, index) => {
                        return (
-                           <div className="col-xs-12 col-sm-6 mb-2" key={index}>
-                               <Chart
-                                 data={chart.serie}
-                                 userConfig={this.props.userConfig}
-                                 titleName={chart.title}
-                               />
-                           </div>
+                        <Col xs={12} sm={6} className="mb-2" key={index}>
+                            <Chart
+                                data={chart.serie}
+                                userConfig={this.props.userConfig}
+                                titleName={chart.title}
+                            />
+                        </Col>
                        );
                    })}
-            </div>
+            </Row>
         );
     }
 }
